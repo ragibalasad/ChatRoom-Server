ec370da5dacfb0127845092faa467de3f6748e18
Ragib Al Asad <ragibalasad@pop-os.localdomain>
2022-01-01 13:00:45 +0600

    modified accessing ip method

--- Server.py
+++ Server.py
@@ -3,7 +3,7 @@ import threading
 
 HEADER = 64
 PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
+SERVER = "0.0.0.0" #socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT) 
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
